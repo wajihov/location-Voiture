682d66a1e67e587d557e2f723caaa4a866c081c6
wajih boufaied <wboufaied@gopartner.fr>
2023-03-13 12:25:39 +0100

    Create project backend

---
+++ src/test/java/fr/gopartner/locationvoiture/LocationVoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gopartner.locationvoiture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVoitureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
